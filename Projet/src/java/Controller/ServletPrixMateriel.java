7bcf0ed9c66b9f32bb131561b020bfa31bc31662
ETU1767 <tsoatsiafoy@gmail.com>
2024-01-25 05:55:21 +0300

    change

--- Projet/src/java/Controller/ServletPrixMateriel.java
+++ Projet/src/java/Controller/ServletPrixMateriel.java
@@ -11,7 +11,6 @@ import Model.Mouvement;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
-
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import jakarta.servlet.RequestDispatcher;
@@ -25,34 +24,6 @@ import jakarta.servlet.http.HttpServletResponse;
  * @author Chan Kenny
  */
 public class ServletPrixMateriel extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ServletPrixMateriel</title>");
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ServletPrixMateriel at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
      *
